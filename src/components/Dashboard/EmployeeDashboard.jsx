41cd179bea1b9793b7e2fbaaf974f87237b14c6d
Deepak Kohli <deepakkohli1810@gmail.com>
2024-12-12 23:45:11 +0530

    minor changes made to the task dashboard

--- src/components/Dashboard/EmployeeDashboard.jsx
+++ src/components/Dashboard/EmployeeDashboard.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Header from '../others/Header'
+import TaskList from '../others/TaskList'
 
 const EmployeeDashboard = () => {
   return (
     <div className='p-10 bg-[#1c1c1c] h-screen'>
         <Header/>
+        <TaskList/>
     </div>
   )
 }
---
+++ src/components/others/TaskList.jsx
@@ -0,0 +1,26 @@
+import React from 'react'
+
+const TaskList = () => {
+  return (
+    <div className='flex justify-between gap-5 screen'>
+      <div className=' w-[45%] bg-red-400 py-6 px-10 rounded-xl mt-10'>
+        <h2 className='text-2xl'>0</h2>
+        <h3 className='text-xl font-medium'>New Task</h3>
+      </div>
+      <div className=' w-[45%] bg-blue-400 py-6 px-10 rounded-xl mt-10'>
+        <h2 className='text-2xl'>0</h2>
+        <h3 className='text-xl font-medium'>New Task</h3>
+      </div>
+      <div className=' w-[45%] bg-green-400 py-6 px-10 rounded-xl mt-10'>
+        <h2 className='text-2xl'>0</h2>
+        <h3 className='text-xl font-medium'>New Task</h3>
+      </div>
+      <div className=' w-[45%] bg-yellow-400 py-6 px-10 rounded-xl mt-10'>
+        <h2 className='text-2xl'>0</h2>
+        <h3 className='text-xl font-medium'>New Task</h3>
+      </div>
+    </div>
+  )
+}
+
+export default TaskList
